b6124e9cbe235f94072d016f992e439f9533f487
Enrico Schulz <107748954+es-you@users.noreply.github.com>
2024-04-05 13:09:49 +0200

    (REBUILD) - The structure of the projects area     * The data now comes from a projects.json, which can be generated from an Excel sheet using the python projectsXLSX_to_json_converter.py script.     * Created a ReadMe_coversion_workflow file - Refactoring index.html     * JavaScript code removed from the structure and packed into separate topic-related files.

---
+++ src/javaScript/ripples.js
@@ -0,0 +1,48 @@
+$(document).ready(function () {
+    try {
+        $('main').ripples({
+            resolution: 1080,
+            dropRadius: 20, //px
+            perturbance: 0.03,
+        });
+
+        $('.ripples-container-large').ripples({
+            resolution: 1080,
+            dropRadius: 10, //px
+            perturbance: 0.03,
+            /*interactive: false*/
+        });
+        $('.ripples-container-small').ripples({
+            resolution: 256,
+            dropRadius: 20, //px
+            perturbance: 1,
+            /*interactive: false*/
+        });
+
+    } catch (e) {
+        $('.error').show().text(e);
+    }
+
+    $('.js-ripples-disable').on('click', function () {
+        $('main, .ripples-container-large, .ripples-container-small').ripples('destroy');
+        $(this).hide();
+    });
+
+    $('.js-ripples-play').on('click', function () {
+        $('header, .ripples-container-large, .ripples-container-small').ripples('play');
+    });
+
+    $('.js-ripples-pause').on('click', function () {
+        $('main, .ripples-container-large, .ripples-container-small').ripples('pause');
+    });
+
+    setInterval(function () {
+        const $el = $('main');
+        const x = Math.random() * $el.outerWidth();
+        const y = Math.random() * $el.outerHeight();
+        const dropRadius = 20;
+        const strength = 0.04 + Math.random() * 0.04;
+
+        $el.ripples('drop', x, y, dropRadius, strength);
+    }, 1000);
+});
